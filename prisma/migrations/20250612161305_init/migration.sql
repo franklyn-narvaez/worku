2bed427082e256e7c86a945d351a2e6041da850f
Fraklyn <franklindonalnarvaezpaz@gmail.com>
2025-06-12 11:30:30 -0500

    setup prisma

---
+++ prisma/migrations/20250612161305_init/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `confirmPassword` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "confirmPassword",
+ALTER COLUMN "status" DROP NOT NULL;
