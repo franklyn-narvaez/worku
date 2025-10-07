6c8518aea7b4407f59e1e58765013d7e8013e355
franklyn-narvaez <franklyn.narvaez@correounivalle.edu.co>
2025-10-07 11:05:42 -0500

    Create list offers, endpoint for students view, application option and view applications
    this commit include the HU WOR-25 Listar ofertas, WOR-27 Aplicar oferta and WOR-36 Listar aplicaciones

---
+++ app/src/modules/students/components/ListOffers.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { STUDENT_OFFER } from "@/constants/path";
+import { useAuth } from "@/hooks/useAuth";
+import type { Offer } from "@prisma/client";
+import { useEffect, useState } from "react";
+import { FormOffer } from "./FormOffer";
+
+type ExtendedOffer = Offer & {
+    college: {
+        id: string;
+        name: string;
+    } | null;
+    faculty: {
+        id: string;
+        name: string;
+    } | null;
+    userApplicationStatus?: "PENDING" | "APPROVED" | "REJECTED" | null;
+};
+
+const ListOffers = () => {
+    const { createAuthFetchOptions } = useAuth();
+
+    const [offers, setOffers] = useState<ExtendedOffer[]>([]);
+
+    useEffect(() => {
+        const fetchOffers = async () => {
+            const fetchOptions = await createAuthFetchOptions();
+            const res = await fetch(STUDENT_OFFER, fetchOptions);
+            const data = await res.json();
+            setOffers(data);
+        };
+        fetchOffers();
+    }, [createAuthFetchOptions]);
+
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-6">Ofertas disponibles</h1>
+
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                {offers.map((offer) => (<FormOffer offer={offer} key={offer.id} />))}
+
+            </div>
+        </div>
+    )
+
+};
+
+export default ListOffers;
